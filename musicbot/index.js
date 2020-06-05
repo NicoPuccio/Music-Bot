1ddf76e87217ebe4c687db078127865b06e6bd48
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2020-06-05 13:13:45 -0300

    Fixed minor errors

--- musicbot/index.js
+++ musicbot/index.js
@@ -10,21 +10,19 @@ client.login(token);
 
 client.once('ready', () => {
     console.log('Ready!');
-    return;
 });
 client.once('reconnecting', () => {
     console.log('Reconnecting!');
-    return;
 });
 client.once('disconnect', () => {
     console.log('Disconnect!');
-    return;
 });
 client.on('message', async message => {
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
 });
 
+const queue = new Map();
 const serverQueue = queue.get(message.guild.id);
 
 if(message.content.startsWith(`${prefix}play`)){
@@ -43,47 +41,49 @@ else{
     message.channel.send('¿Te cuesta escribir bien los comandos no? Me imagino como seras jugando, manco');
 }
 
-const queue = new Map();
-async function execute(message, serverQueue);
-const args = message.content.split(' ');
-const isInVoiceChannel = message.member.voiceChannel;
-if(!isInVoiceChannel) return channel.message.send('Necesitas estar en un canal de voz para poner play bro');
-const permissions = voiceChannel.permissionsFor(message.client.user);
-if(!permissions.has('CONNECT') || !permissions.has('SPEAK')) return channel.message.send("Necesito que me des permisos para unirme y hablar en el server, maestro");
+    async function execute(message, serverQueue){
+    const args = message.content.split(' ');
+    const isInVoiceChannel = message.member.voiceChannel;
+    if(!isInVoiceChannel) return channel.message.send('Necesitas estar en un canal de voz para poner play bro');
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if(!permissions.has('CONNECT') || !permissions.has('SPEAK')) return channel.message.send("Necesito que me des permisos para unirme y hablar en el server, maestro");
 
-const songInfo = await ytdl.getInfo(args[1]); //the splitted message has in the second argument the link
-const song = {
-    tittle: songInfo.title,
-    url: songInfo.url
-};
+    const songInfo = await ytdl.getInfo(args[1]); //the splitted message has in the second argument the link
+    const song = {
+        tittle: songInfo.title,
+        url: songInfo.url
+     };
 
-if(!serverQueue){
 
-}else{
-    serverQueue.songs.push(song);
-    console.log(serverQueue.songs);
-    return message.channel.send(`${song.title} se agrego a la playlist`);
-}
+    if(!serverQueue){
 
-const queueContract = {
-    textChannel: message.channel,
-    voiceChannel: voiceChannel,
-    connection: null,
-    songs: [],
-    volume: 5,
-    playing: true
-};
-queue.set(message.guild.id, queueContract);
-queueContract.songs.push(song);
+    } else{
+        serverQueue.songs.push(song);
+        console.log(serverQueue.songs);
+        return message.channel.send(`${song.title} se agrego a la playlist`);
+     }
 
-try{
-    var connection = await voiceChannel.join();
-    queueContract.connection = connection;
-    play(message.guild, queueContract.songs[0]);
-} catch (err){
-    console.log(err);
-    queue.delete(message.guild.id);
-    return message.channel.send(err);
+
+    const queueContract = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+    };
+    queue.set(message.guild.id, queueContract);
+    queueContract.songs.push(song);
+
+    try{
+        var connection = await voiceChannel.join();
+        queueContract.connection = connection;
+        play(message.guild, queueContract.songs[0]);
+    } catch (err){
+        console.log(err);
+        queue.delete(message.guild.id);
+        return message.channel.send(err);
+    }
 }
 
 //play a song
